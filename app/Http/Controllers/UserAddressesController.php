85c7a130404a41ebd80c1991cd07168331e55c2a
LiVanke <2826408771@qq.com>
2019-09-15 17:13:01 +0800

    新增收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -3,14 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserAddress;
+use App\Http\Requests\UserAddressRequest;
 
 class UserAddressesController extends Controller
 {
-    //
+    //收获地址列表
     public function index(Request $request)
     {
         return view('user_addresses.index', [
             'addresses' => $request->user()->addresses,
         ]);
+    }
+
+    //添加收货地址
+    public function create()
+    {
+        return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
+    }
+
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->addresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
     }
 }
