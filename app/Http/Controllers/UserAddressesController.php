29b9a6317140c5b61c235babf505cb37a576382b
LiVanke <2826408771@qq.com>
2019-09-15 20:58:20 +0800

    修改和删除收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -8,7 +8,7 @@ use App\Http\Requests\UserAddressRequest;
 
 class UserAddressesController extends Controller
 {
-    //收获地址列表
+    //收获地址列表页面
     public function index(Request $request)
     {
         return view('user_addresses.index', [
@@ -16,12 +16,13 @@ class UserAddressesController extends Controller
         ]);
     }
 
-    //添加收货地址
+    //添加收货地址页面
     public function create()
     {
         return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
     }
 
+    //提交添加收货地址
     public function store(UserAddressRequest $request)
     {
         $request->user()->addresses()->create($request->only([
@@ -35,5 +36,38 @@ class UserAddressesController extends Controller
         ]));
 
         return redirect()->route('user_addresses.index');
+    }
+
+    //修改收货地址页面
+    public function edit(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        return view('user_addresses.create_and_edit', ['address' => $user_address]);
+    }
+
+    //提交修改收货地址
+    public function update(UserAddress $user_address, UserAddressRequest $request)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->update($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
+    }
+
+    //提交删除收货地址
+    public function destroy(UserAddress $user_address)
+    {
+        $this->authorize('own', $user_address);
+        $user_address->delete();
+
+        return [];
     }
 }
