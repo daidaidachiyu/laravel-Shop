c38dc1c7c9de86c40193ff31a855e4f8aff8aaba
LiVanke <2826408771@qq.com>
2019-09-18 20:50:56 +0800

    管理后台 - 新增、编辑商品

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class ProductSku extends Model
 {
     //
+    protected $fillable = ['title', 'description', 'price', 'stock'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
