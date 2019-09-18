2fd811a12a46672cadc96acd4957882d3ba44aa4
LiVanke <2826408771@qq.com>
2019-09-18 22:20:45 +0800

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,8 @@
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
